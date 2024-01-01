cbd6b5f8953bebc159cb1e91aa6c3ba8b9d88878
chloeeee210 <chloeseo0210@gmail.com>
2024-01-01 15:04:01 +0900

    global.css 왜지웠냐 ㅋㅋㅋㅋㅋㅋㅋㅋ

--- breakable-toy/src/main.tsx
+++ breakable-toy/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {CardDemo} from "./CardDemo.tsx";
+import "./assets/global.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
